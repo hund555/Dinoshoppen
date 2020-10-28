e336477be5bd326ad20cb6ac3fc3e998b61d5c95
Allan Holm Iversen <alla1862@elevcampus.dk>
2020-10-28 09:49:23 +0100

    Ajusted text, you can now add new users and updated README

---
+++ DinosaurShoppen/Pages/CustomerPages/CreateCustomer.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DataLayer;
+using DataLayer.Entities;
+using ServiceLayer.CustomerService.Services.Interfaces;
+using ServiceLayer.CustomerService.DTOCollection;
+
+namespace DinosaurShoppen.Pages.CustomerPages
+{
+    public class CreateCustomerModel : PageModel
+    {
+        private readonly ICustomerServiceAdmin _customerService;
+
+        public CreateCustomerModel(ICustomerServiceAdmin customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public FullCustomerDTO Customer { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _customerService.AddNewCustomer(Customer);
+
+            return RedirectToPage("./Login");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,15 +8,15 @@ Jeg har fået til opgave at lave en E Shop, hvor jeg har valgt at lave en dinosau
 - [x] Hente 6 dinosaurere som har en promotion
 - [x] Hente alle dinosaurere til en liste
 - [x] Hente detaljer om dinosaur på id
-- [ ] Slette en dinosaur
-- [ ] Updatere en dinosaur
-- [ ] Lave en ny promotion
-- [ ] Oprette en ny kunde
+- [x] Slette en dinosaur
+- [x] Updatere en dinosaur
+- [x] Lave en ny promotion
+- [x] Oprette en ny kunde
 - [x] Hente en kundes kurv
 - [x] Slette fra en kurv
-- [ ] Oprette en rabat som kunder kan bruge
-- [ ] Fjerne promotioner
-- [ ] Fjerne rabatter
+- [x] Oprette en rabat som kunder kan bruge
+- [x] Fjerne promotioner
+- [x] Fjerne rabatter
 - [ ] Fjerne kunder
 - [x] Tilføje rabat til kundes kurv
 - [x] Page sorter og filter på dinosaur listen
